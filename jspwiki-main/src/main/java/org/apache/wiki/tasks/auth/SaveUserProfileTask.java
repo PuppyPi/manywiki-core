5e1cff648be85bbfe375c86398ce80a62eb1a904
PuppyPi <puppyofcodez@gmail.com>
2022-06-13 07:10:36 +0000

    No hardcoded emailing people in the core! X'D No dependencies on email libraries at all! Let the configuring outer code decide what to do on event notifications (email, sms, database logging, filesystem logging, special standardized opt-in system instead of email with phone apps and clients that should exist but I'm not aware of X'D, nothing, etc.! )

--- jspwiki-main/src/main/java/org/apache/wiki/tasks/auth/SaveUserProfileTask.java
+++ jspwiki-main/src/main/java/org/apache/wiki/tasks/auth/SaveUserProfileTask.java
@@ -14,8 +14,8 @@ import org.apache.wiki.workflow.Outcome;
 import org.apache.wiki.workflow.Task;
 import org.apache.wiki.workflow.WorkflowManager;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
+//import javax.mail.MessagingException;
+//import javax.mail.internet.AddressException;
 import java.util.Locale;
 
 
@@ -23,58 +23,59 @@ import java.util.Locale;
  * Handles the actual profile save action. 
  */
 public class SaveUserProfileTask extends Task {
-
-    private static final long serialVersionUID = 6994297086560480285L;
-    private static final Logger LOG = LogManager.getLogger( SaveUserProfileTask.class );
-    private final Locale m_loc;
-
-    /**
-     * Constructs a new Task for saving a user profile.
-     */
-    public SaveUserProfileTask( final Locale loc ) {
-        super( TasksManager.USER_PROFILE_SAVE_TASK_MESSAGE_KEY );
-        m_loc = loc;
-    }
-
-    /**
-     * Saves the user profile to the user database.
-     *
-     * @return {@link org.apache.wiki.workflow.Outcome#STEP_COMPLETE} if the task completed successfully
-     * @throws WikiException if the save did not complete for some reason
-     */
-    @Override
-    public Outcome execute( final Context context ) throws WikiException {
-        // Retrieve user profile
-        final UserProfile profile = ( UserProfile )getWorkflowContext().get( WorkflowManager.WF_UP_CREATE_SAVE_ATTR_SAVED_PROFILE );
-
-        // Save the profile (userdatabase will take care of timestamps for us)
-        context.getEngine().getManager( UserManager.class ).getUserDatabase().save( profile );
-
-        // Send e-mail if user supplied an e-mail address
-        if ( profile != null && profile.getEmail() != null ) {
-            try {
-                final InternationalizationManager i18n = context.getEngine().getManager( InternationalizationManager.class );
-                final String app = context.getEngine().getApplicationName();
-                final String to = profile.getEmail();
-                final String subject = i18n.get( InternationalizationManager.DEF_TEMPLATE, m_loc,
-                                                 "notification.createUserProfile.accept.subject", app );
-
-                final String loginUrl = context.getEngine().getURL( ContextEnum.WIKI_LOGIN.getRequestContext(), null, null );
-                final String content = i18n.get( InternationalizationManager.DEF_TEMPLATE, m_loc,
-                                                 "notification.createUserProfile.accept.content", app,
-                                                 profile.getLoginName(),
-                                                 profile.getFullname(),
-                                                 profile.getEmail(),
-                                                 loginUrl );
-                MailUtil.sendMessage( context.getEngine().getWikiProperties(), to, subject, content );
-            } catch ( final AddressException e) {
-                LOG.debug( e.getMessage(), e );
-            } catch ( final MessagingException me ) {
-                LOG.error( "Could not send registration confirmation e-mail. Is the e-mail server running?", me );
-            }
-        }
-
-        return Outcome.STEP_COMPLETE;
-    }
-
+	
+	private static final long serialVersionUID = 6994297086560480285L;
+	private static final Logger LOG = LogManager.getLogger( SaveUserProfileTask.class );
+	private final Locale m_loc;
+	
+	/**
+	 * Constructs a new Task for saving a user profile.
+	 */
+	public SaveUserProfileTask( final Locale loc ) {
+		super( TasksManager.USER_PROFILE_SAVE_TASK_MESSAGE_KEY );
+		m_loc = loc;
+	}
+	
+	/**
+	 * Saves the user profile to the user database.
+	 *
+	 * @return {@link org.apache.wiki.workflow.Outcome#STEP_COMPLETE} if the task completed successfully
+	 * @throws WikiException if the save did not complete for some reason
+	 */
+	@Override
+	public Outcome execute( final Context context ) throws WikiException {
+		// Retrieve user profile
+		final UserProfile profile = ( UserProfile )getWorkflowContext().get( WorkflowManager.WF_UP_CREATE_SAVE_ATTR_SAVED_PROFILE );
+		
+		// Save the profile (userdatabase will take care of timestamps for us)
+		context.getEngine().getManager( UserManager.class ).getUserDatabase().save( profile );
+		
+		//RPMOD — good lord Things That Happen (if not "Events"?XD) should be handled in a universal way without the core depending on one, completely aribtrary, thing to do with them!  (the core shouldn't even depende on javax.mail!)
+		// Send e-mail if user supplied an e-mail address
+		//        if ( profile != null && profile.getEmail() != null ) {
+		//            try {
+		//                final InternationalizationManager i18n = context.getEngine().getManager( InternationalizationManager.class );
+		//                final String app = context.getEngine().getApplicationName();
+		//                final String to = profile.getEmail();
+		//                final String subject = i18n.get( InternationalizationManager.DEF_TEMPLATE, m_loc,
+		//                                                 "notification.createUserProfile.accept.subject", app );
+		//
+		//                final String loginUrl = context.getEngine().getURL( ContextEnum.WIKI_LOGIN.getRequestContext(), null, null );
+		//                final String content = i18n.get( InternationalizationManager.DEF_TEMPLATE, m_loc,
+		//                                                 "notification.createUserProfile.accept.content", app,
+		//                                                 profile.getLoginName(),
+		//                                                 profile.getFullname(),
+		//                                                 profile.getEmail(),
+		//                                                 loginUrl );
+		//                MailUtil.sendMessage( context.getEngine().getWikiProperties(), to, subject, content );
+		//            } catch ( final AddressException e) {
+		//                LOG.debug( e.getMessage(), e );
+		//            } catch ( final MessagingException me ) {
+		//                LOG.error( "Could not send registration confirmation e-mail. Is the e-mail server running?", me );
+		//            }
+		//        }
+		
+		return Outcome.STEP_COMPLETE;
+	}
+	
 }

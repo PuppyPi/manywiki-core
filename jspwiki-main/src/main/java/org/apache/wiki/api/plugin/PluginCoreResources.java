68d280b552c0601a1a07d9310424b14d46f11e2e
PuppyPi <puppyofcodez@gmail.com>
2022-06-16 20:24:43 +0000

    (forgot this important one ^^' )

---
+++ jspwiki-main/src/main/java/org/apache/wiki/api/plugin/PluginCoreResources.java
@@ -0,0 +1,10 @@
+package org.apache.wiki.api.plugin;
+
+import rebound.annotations.semantic.meta.dependencies.DependencyDirectory;
+
+@DependencyDirectory("./res")
+public class PluginCoreResources
+{
+	/** Name of the default plugin resource bundle. */
+	public static final String CORE_PLUGINS_RESOURCEBUNDLE = "org.apache.wiki.api.plugin.res.PluginResources";
+}

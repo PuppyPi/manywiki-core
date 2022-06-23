47c20696a67180153c08429e6bd1723161e0b893
PuppyPi <puppyofcodez@gmail.com>
2022-06-23 18:08:38 +0000

    Java code removed from alllllllll the JSPsssssss! :D (Not including tagfiles ^^' )
    Wow that was easy! :DD

---
+++ srcs/main/net/manywiki/jee/actions/other/closet/CodeFromReaderTemplate.java
@@ -0,0 +1,32 @@
+package net.manywiki.jee.actions.other.closet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.wiki.api.core.Context;
+import org.apache.wiki.api.core.ContextEnum;
+import org.apache.wiki.api.spi.Wiki;
+import net.manywiki.jee.actions.ManyWikiActionBean;
+
+public class CodeFromReaderTemplate
+extends ManyWikiActionBean
+{
+	@Override
+	protected void doLogic() throws ServletException, IOException
+	{
+		HttpServletRequest request = getRequest();
+		HttpServletResponse response = getResponse();
+		HttpSession session = request.getSession();
+		
+	    // Create wiki context and check for authorization
+		ContextEnum cte = ContextEnum.PAGE_COMMENT;
+	    Context wikiContext = Wiki.context().create( engine, request, cte.getRequestContext() );
+	    
+	    // ...
+	    
+		setVariableForJSPView("wikiJsonUrl", wikiContext.getURL( ContextEnum.PAGE_NONE.getRequestContext(), "ajax" ));
+        serveJSPView("/templates/reader/ViewTemplate.jsp");
+	}
+}

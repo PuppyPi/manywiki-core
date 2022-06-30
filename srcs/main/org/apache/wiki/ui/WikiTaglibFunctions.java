55e7bf8eb743cbc6e405a4fd3de89433ea0a041e
PuppyPi <puppyofcodez@gmail.com>
2022-06-30 13:37:57 +0000

    Let's keep it in the family XD

--- srcs/main/org/apache/wiki/ui/WikiTaglibFunctions.java
+++ srcs/main/org/apache/wiki/ui/WikiTaglibFunctions.java
@@ -4,6 +4,11 @@ import org.apache.wiki.api.core.Page;
 
 public class WikiTaglibFunctions
 {
+	public static String escapeHTML(String text)
+	{
+		return org.apache.wiki.util.TextUtil.replaceEntities(text);
+	}
+	
 	public static String formatBytes(long bytes)
 	{
 		return org.apache.commons.io.FileUtils.byteCountToDisplaySize(bytes);
